a04cb8d7e8b776945852fe7b8263428113bfa80a
GreysonJohns <greyson.johns@stu.boone.kyschools.us>
2021-05-10 12:30:51 -0400

    Edits 5/10/21
    Added Start Screen and Transition to Tutorial. Added Sprite for Orb Basic.
    
    Plans to use checkered movement.

---
+++ Intro.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Intro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Intro extends World
+{
+
+    /**
+     * Constructor for objects of class Intro.
+     * 
+     */
+    public Intro()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        showText("Orbus The Beginning", 300, 100);
+        showText("Press ENTER to Start", 300, 200);
+        
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("ENTER"))
+        {
+            Greenfoot.setWorld(new Tutorial());
+        }
+    }
+}
